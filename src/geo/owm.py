290945d0a153943602d8187f98dc74d9e3ae8c79
manucorsu <142500898+manucorsu@users.noreply.github.com>
2026-05-29 11:38:19 -0300

    creo que está todo geocode, sin testeare (en 5 minutos lo estará)

--- src/geo/owm.py
+++ src/geo/owm.py
@@ -4,6 +4,7 @@ from numbers import Number
 from ..types import Ciudad
 from ..secrets import owm_api_key
 from . import geocache
+from json import JSONDecodeError
 
 assert owm_api_key is not None  # main no permite que este módulo llegue a abrirse
 # si no está la API key (cierra todo en el momento), esto es solo para satisfacer
@@ -17,7 +18,7 @@ assert owm_api_key is not None  # main no permite que este módulo llegue a abri
 # Investigando un poco más, esta versión dejó de ser soportada hace casi dos años en
 # junio de 2024 (https://openweathermap.org/api/one-call-transfer), por lo que
 # decidimos seguir lo que recomienda la documentación actual y usar la versión estable
-# más nueva, la 4.0 incluso si eso implicaba tener que realizar algún paso de más.
+# más nueva, la 4.0, incluso si eso implicaba tener que realizar algún paso de más.
 
 BASE_URL_CLIMA = "https://api.openweathermap.org/data/4.0/onecall/current"
 UNITS_CLIMA = "metric"
@@ -29,59 +30,124 @@ BASE_URL_GEOCODING = "https://api.openweathermap.org/geo/1.0/direct"
 LIMIT_GEOCODING = 1
 
 
-def geocode_nombre_ciudad(ciudad: str) -> Ciudad:
-    # TODO: handling a la javier apat
-    print("Buscando la ciudad...")
+def handle_owm_errors(
+    ex: (
+        IndexError
+        | requests.exceptions.HTTPError
+        | requests.exceptions.RequestException
+        | JSONDecodeError
+    ),
+    res: requests.Response | None,
+    n_cit: str | None = None,
+) -> None:
+    # basado en página 8 de la consigna
+    print("❌.")
+
+    def _handle_404():
+        print(
+            f"Error OWM: Ciudad '{n_cit}' no encontrada. Verificá que esté bien escrito el nombre."
+        )
+        # podemos usar n_cit porque solo el geocoding puede
+        # "tirar 404" (IndexError)
+
+    if isinstance(ex, IndexError):
+        return _handle_404()
+    elif isinstance(ex, requests.exceptions.HTTPError):
+        if res is None:
+            print(f"Error HTTP OWM: {ex}")
+            return
+        match (res.status_code):
+            case 401:
+                print(
+                    "Error de autenticación OWM: API Key inválida, es decir, el valor de OWM_API_KEY en .env es incorrecto. Si aún deseas usar las funciones de búsqueda de clima, tenés que cerrar la aplicación, colocar el valor correcto y volverla a abrir."
+                )
+                return
+            case 404:
+                return _handle_404()
+            case _:
+                print(f"Error HTTP OWM: {ex}")
+                return
+    elif isinstance(ex, requests.exceptions.RequestException):
+        print(f"Error de conexión/petición OWM: {ex}")
+        return
+    else:  # (JSONDecodeError)
+        print("Error OWM: La respuesta de la API no es JSON válido")
+        return
+
+
+def geocode_nombre_ciudad(ciudad: str) -> Ciudad | None:
+    # En cumplimiento con el handling propuesto en la consigna (que es para la versión 2.5 que no requiere este paso),
+    # devuelve None si no se encuentra ninguna ciudad o encuentra un error, print()eando adecuadamente
+    print("Buscando la ciudad", end="")
     c_en_cache = geocache.ciudad_en_cache(ciudad)
     if c_en_cache:
-        print("La ciudad estaba cacheada, devolviendo lat/lon...")
+        print("...✅ Encontrada en caché.")
         return c_en_cache
-    print("Buscando la ciudad por geocoding...")
+    print("por geocoding...", end="")
     parametros: dict[str, Any] = {
         "q": ciudad,
         "appid": owm_api_key,
         "limit": LIMIT_GEOCODING,
     }
 
-    respuesta = requests.get(BASE_URL_GEOCODING, params=parametros, timeout=TIMEOUT)
-    respuesta.raise_for_status()
     datos_ciudad = None
+    respuesta = None
     try:
+        respuesta = requests.get(BASE_URL_GEOCODING, params=parametros, timeout=TIMEOUT)
+        respuesta.raise_for_status()
         datos_ciudad = (respuesta.json())[0]
-    except (
-        KeyError
-    ):  # Cuando no encuentra nada devuelve un array vacío con status 200, no 404
-        raise ValueError(
-            f"No se encontró una ciudad de nombre '{ciudad}', revisá que esté bien escrito."
-        )
+        # cuando no encuentra nada, no devuelve un error con 404 sino que
+        # con el status 200 devuelve una lista vacía, de esta forma si
+        # lo está levantamos un IndexError que handleamos como pide la
+        # consigna que se handleen los 404
 
+    except (
+        IndexError,
+        requests.exceptions.HTTPError,
+        requests.exceptions.RequestException,
+        JSONDecodeError,
+    ) as ex:
+        return handle_owm_errors(ex, respuesta, ciudad)
     nom = None
     try:
-        nom = datos_ciudad["local_names"]["es"]
-    except KeyError:
-        nom = datos_ciudad["name"]
-    assert isinstance(nom, str)
-    # Primero intenta buscar el nombre en español, si no está agarra el
-    # "name" genérico en inglés que siempre está presente.
-    # Todos los asserts en esta función (y en general en todo el proyecto) nunca
-    # resultarán en AssertionError y están solo para que se conserven los tipos
-    # como una ayuda de escritura para nosotros.
-    pais = datos_ciudad["country"]
-    assert isinstance(pais, str)
-    nombre_completo = f"{nom}, {pais}"
+        english_name = datos_ciudad["name"]  # Toda respuesta válida lo tiene
+        local_names = datos_ciudad.get("local_names")
+        nombre_es = None
+        if local_names:
+            nombre_es = local_names.get("es")
 
-    lat = datos_ciudad["lat"]
-    assert isinstance(lat, Number)
+        if not local_names or not nombre_es:
+            nom = english_name
+        else:
+            nom = nombre_es
+        assert isinstance(nom, str)
+        # Primero intenta buscar el nombre en español, si no está agarra el
+        # "name" genérico en inglés que siempre está presente.
+        # Todos los asserts en esta función (y en general en todo el proyecto) nunca
+        # resultarán en AssertionError y están solo para que se conserven los tipos
+        # como una ayuda de escritura para nosotros.
+        pais = datos_ciudad["country"]
+        assert isinstance(pais, str)
+        nombre_completo = f"{nom}, {pais}"
 
-    lon = datos_ciudad["lon"]
-    assert isinstance(lon, Number)
+        lat = datos_ciudad["lat"]
+        assert isinstance(lat, Number)
 
-    nu = ciudad.upper()
-    nueva_c: Ciudad = {
-        "nombre_completo": nombre_completo,
-        "lat": lat,
-        "lon": lon,
-        "otros_nombres": [nu] if nu != nombre_completo.upper() else [],
-    }
-    geocache.agregar_ciudad_nueva(nueva_c)
-    return nueva_c
+        lon = datos_ciudad["lon"]
+        assert isinstance(lon, Number)
+
+        nu = ciudad.upper()
+        nueva_c: Ciudad = {
+            "nombre_completo": nombre_completo,
+            "lat": lat,
+            "lon": lon,
+            "otros_nombres": [nu] if nu != nombre_completo.upper() else [],
+        }
+        geocache.agregar_ciudad_nueva(nueva_c)
+        print("✅ Encontrada.")
+        return nueva_c
+    except (KeyError, AssertionError, TypeError, AttributeError) as ex:
+        print(
+            f"❌.\nError: formato inesperado de datos de OWM geocoding. Más info.: {ex}"
+        )
+        return
